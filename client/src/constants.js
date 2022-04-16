e90418475c95112bc43248ab6398ddc31ddff279
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-04-16 01:13:59 +0100

    ch: updated CoinPot contract address.

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 export const ERC20_DECIMALS = 18;
 export const cUSDContractAddress = '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1';
-export const CPContractAddress = '0xa848846255a447E7BDD73F0656e6f85E35a8c42d';
+export const CPContractAddress = '0x466332B6B52ad2464CBb78D8c164029cA9E43079';
