d99e7885846d3706a7042f9e5209d843691f183f
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-04-16 01:50:01 +0100

    bg: fixed empty lottery winners bug. ch: replaced ccoinpot contract address.

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 export const ERC20_DECIMALS = 18;
 export const cUSDContractAddress = '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1';
-export const CPContractAddress = '0x466332B6B52ad2464CBb78D8c164029cA9E43079';
+export const CPContractAddress = '0x0A2e51C2053a87c891a1148D7402B9c74DD80296';
