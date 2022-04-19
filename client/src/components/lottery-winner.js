7eec846fcbb51a0df3ae7ef23c5de3a767d09a5a
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-04-19 07:30:23 +0100

    ft: added winner address to winners component.

--- client/src/components/lottery-winner.js
+++ client/src/components/lottery-winner.js
@@ -1,25 +1,27 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, VStack } from '@chakra-ui/react';
 import Blockies from 'react-blockies';
 
 const LotteryWinner = ({ address, amount, timestamp }) => {
   return (
-    <Flex
-      alignItems="center"
-      justifyContent="space-between"
-      flexWrap="wrap"
-      w="full"
-      px="5"
-      pt="5"
-      pb="2"
-    >
-      <Box mr="3" mb="3">
-        <Blockies seed={address} size={8} scale={4} />
-      </Box>
-      <Text mr="3" mb="3">
-        ${amount}
+    <VStack px="5" pt="5" pb="7" w="full">
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        flexWrap="wrap"
+        w="full"
+      >
+        <Box mr="3" mb="3">
+          <Blockies seed={address} size={8} scale={4} />
+        </Box>
+        <Text mr="3" mb="3">
+          ${amount}
+        </Text>
+        <Text mb="3">{timestamp.toUTCString()}</Text>
+      </Flex>
+      <Text textAlign="left" w="full">
+        {address}
       </Text>
-      <Text mb="3">{timestamp.toUTCString()}</Text>
-    </Flex>
+    </VStack>
   );
 };
 
